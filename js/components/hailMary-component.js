2da6dd2a57575b71e78958939444c13444f945da
Steven Simon <stevensimon@Stevens-iMac.local>
2026-01-13 03:29:22 -0600

    Fixed hail mary text again

--- js/components/hailMary-component.js
+++ js/components/hailMary-component.js
@@ -17,10 +17,10 @@ class HailMaryComponent extends HTMLElement {
             this.innerHTML = `
                 <h2>Hail Mary</h2>
                 <p class="behindPhotoParagraph">
-                    Hail Mary, full of grace. The Lord is with you.<br>
-                    Blessed are you among women and blessed is the fruit of your womb, Jesus.<br>
+                    Hail Mary, full of grace, the Lord is with thee.<br>
+                    Blessed art thou among women, and blessed is the fruit of thy womb, Jesus.<br>
                     <strong>Holy Mary, Mother of God, pray for us sinners,<br>
-                    spread the effect of grace of your Flame of Love over all of humanity<br>
+                    spread the effect of grace of thy Flame of Love over all of humanity<br>
                     now and at the hour of our death. Amen.</strong>
                 </p>
             `;
